43e9902b9eedbe916d19efc9a527ce95eca24b53
Veronika Emelyanchenko <v.emelyanchenko@gmail.com>
2026-02-09 12:55:59 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by veronika on 9.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
