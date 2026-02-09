4b350c3c0ffe3e2cf70b413c352b35dbf3f0b9a1
Veronika Emelyanchenko <v.emelyanchenko@gmail.com>
2026-02-09 13:21:38 +0300

    Add element to the code

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var changeSignalButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        redSignalView.layer.cornerRadius = 70
+        yellowSignalView.layer.cornerRadius = 70
+        greenSignalView.layer.cornerRadius = 70
+        changeSignalButton.layer.cornerRadius = 15
 
+    }
+    
 
 }
 
